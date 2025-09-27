fc3c077c1f0ad1c54bf2a7ebdf845b5ce9b4b474
nuttapongmalawech-sys <nuttapong.malawech@gmail.com>
2025-09-27 17:03:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("YOLO Image Detection App :)")
 
 
 # Load YOLO model
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8n (1).pt")
 # model = YOLO("yolo11n.pt")
 # Upload image
 uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jpeg", "png"])
